33ac12e202d7da43f882f8ab75296248ca5fc45f
24f3002795 <24f3002795@ds.study.iitm.ac.in>
2026-07-11 18:34:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from openai import OpenAI
-
+from fastapi import HTTPException
 client = OpenAI()
 
 app = FastAPI()
@@ -29,37 +29,42 @@ def root():
     return {"status": "ok"}
 
 
+
 @app.post("/answer-image")
 def answer(req: Request):
+    try:
+        image_url = f"data:image/png;base64,{req.image_base64}"
 
-    image_url = f"data:image/png;base64,{req.image_base64}"
-
-    response = client.chat.completions.create(
-        model="gpt-4.1-mini",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": (
-                            "Answer ONLY the question from the image. "
-                            "Return only the answer. "
-                            "If numeric, return only the number as a string."
-                            f"\nQuestion: {req.question}"
-                        ),
-                    },
-                    {
-                        "type": "image_url",
-                        "image_url": {
-                            "url": image_url
+        response = client.chat.completions.create(
+            model="gpt-4.1-mini",
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": (
+                                "Answer ONLY the question from the image.\n"
+                                "Return only the answer.\n"
+                                "If numeric return only the number.\n"
+                                f"Question: {req.question}"
+                            ),
                         },
-                    },
-                ],
-            }
-        ],
-    )
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": image_url
+                            },
+                        },
+                    ],
+                }
+            ],
+        )
 
-    ans = response.choices[0].message.content.strip()
+        return {
+            "answer": response.choices[0].message.content.strip()
+        }
 
-    return {"answer": ans}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
